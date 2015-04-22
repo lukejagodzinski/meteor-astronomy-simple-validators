e29d2fce24f329fd9aab53b922044d8a6f9812eb
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-04-23 00:43:47 +0200

    First sketches

---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: 'jagi:astronomy-simple-validators',
+  version: '0.1.0',
+  summary: 'Simple validators for Meteor Astronomy',
+  git: 'https://github.com/jagi/meteor-astronomy-simple-validators.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+
+  api.use('jagi:astronomy@0.2.0');
+  api.use('jagi:astronomy-validators@0.2.0');
+  api.use('underscore');
+
+  // Module.
+  api.addFiles('lib/module/schema.js', ['client', 'server']);
+  api.addFiles('lib/module/parser.js', ['client', 'server']);
+  api.addFiles('lib/module/module.js', ['client', 'server']);
+});
