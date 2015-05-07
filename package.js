de3e6503486f7886660405591709bae48337445a
jagi <luke.jagodzinski@gmail.com>
2015-05-07 15:03:06 +0200

    New events system

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jagi:astronomy-simple-validators',
-  version: '0.2.1',
+  version: '0.3.0',
   summary: 'Simple validators for Meteor Astronomy',
   git: 'https://github.com/jagi/meteor-astronomy-simple-validators.git'
 });
@@ -8,8 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('jagi:astronomy@0.4.1');
-  api.use('jagi:astronomy-validators@0.4.1');
+  api.use('jagi:astronomy-validators@0.5.0');
   api.use('underscore');
 
   // Module.
