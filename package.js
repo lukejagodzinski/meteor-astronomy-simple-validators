e86ce8146f93d891f51f34929111d3a5c027ad38
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-05-17 03:20:34 +0200

    Moving methods from schema to class

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jagi:astronomy-simple-validators',
-  version: '0.3.2',
+  version: '0.4.0',
   summary: 'Simple validators for Meteor Astronomy',
   git: 'https://github.com/jagi/meteor-astronomy-simple-validators.git'
 });
@@ -8,12 +8,11 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('jagi:astronomy-validators@0.6.2');
+  api.use('jagi:astronomy-validators@0.7.0');
   api.use('underscore');
 
   // Module.
   api.addFiles('lib/module/parser.js', ['client', 'server']);
-  api.addFiles('lib/module/init_module.js', ['client', 'server']);
-  api.addFiles('lib/module/init_schema.js', ['client', 'server']);
+  api.addFiles('lib/module/init_class.js', ['client', 'server']);
   api.addFiles('lib/module/module.js', ['client', 'server']);
 });
