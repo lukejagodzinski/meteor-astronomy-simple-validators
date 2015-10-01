ebdf134141dc3552149595830767df41e3824520
jagi <luke.jagodzinski@gmail.com>
2015-10-01 14:12:31 +0200

    Bump version to 1.0.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jagi:astronomy-simple-validators',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'Simple validators for Meteor Astronomy',
   git: 'https://github.com/jagi/meteor-astronomy-simple-validators.git'
 });
